9c237b026e56f57a32b3ae21b0daac4b787dab09
Dmytro Molchanov <dima-25@ukr.net>
2023-01-19 13:37:52 +0200

    Create migrations

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiiproject',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=yiiproject',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
---
+++ basic/migrations/m230119_113534_create_topic_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%topic}}`.
+ */
+class m230119_113534_create_topic_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%topic}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%topic}}');
+    }
+}
